63206e6f12ec5eb5bb5495690fbe2bdb051b4797
Mark Ciril <markciril.jamon@gmail.com>
2026-04-13 20:59:40 +0800

    chore: update favicon from dev.png to timetrack-pro-icon.png

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="icon" href="{{ asset('assets/dev.png') }}" type="image/png">
+  <link rel="icon" href="{{ asset('assets/timetrack-pro-icon.png') }}" type="image/png">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @vite(['resources/css/app.css', 'resources/js/app.js'])
