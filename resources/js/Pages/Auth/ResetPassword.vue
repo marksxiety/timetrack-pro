d7f9f2274cbfa6eb8f055a47cde1290e9678c5da
Mark Ciril <markciril.jamon@gmail.com>
2026-04-15 22:02:56 +0800

    ref: replace inline SVGs with Iconify icons in reset password page

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -16,11 +16,7 @@
             <div class="text-center space-y-1">
                 <div class="flex items-center justify-center gap-2 mb-2">
                     <div class="w-9 h-9 rounded-lg bg-primary flex items-center justify-center shadow-lg">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-primary-content" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" fill="none" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6l4 2" />
-                        </svg>
+                        <Icon icon="material-symbols:schedule-outline" width="20" height="20" class="text-primary-content" />
                     </div>
                     <h1 class="text-3xl font-black text-base-content tracking-tight">TimeTrack <span
                             class="text-primary">Pro</span></h1>
@@ -36,11 +32,7 @@
                     <!-- Icon -->
                     <div class="flex justify-center mb-2">
                         <div class="w-16 h-16 rounded-2xl bg-primary/10 flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-primary" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
-                            </svg>
+                            <Icon icon="material-symbols:key-outline" width="32" height="32" class="text-primary" />
                         </div>
                     </div>
 
@@ -65,27 +57,14 @@
                             </label>
                             <label
                                 class="input input-bordered flex items-center gap-2 focus-within:input-primary transition-all w-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-base-content/40 shrink-0"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
-                                </svg>
+                                <Icon icon="material-symbols:lock-outline" width="16" height="16"
+                                    class="text-base-content/40 shrink-0" />
                                 <input :type="showPassword ? 'text' : 'password'" class="grow bg-transparent outline-none text-sm"
                                     placeholder="Enter new password" v-model="form.password" />
                                 <button type="button" @click="showPassword = !showPassword"
                                     class="text-base-content/30 hover:text-base-content/60 transition-colors">
-                                    <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-                                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
-                                    </svg>
+                                    <Icon v-if="!showPassword" icon="material-symbols:visibility-outline" width="16" height="16" />
+                                    <Icon v-else icon="material-symbols:visibility-off-outline" width="16" height="16" />
                                 </button>
                             </label>
                             <span v-if="form.errors.password" class="text-error text-xs mt-0.5">{{ form.errors.password
@@ -101,28 +80,15 @@
                             </label>
                             <label
                                 class="input input-bordered flex items-center gap-2 focus-within:input-primary transition-all w-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-base-content/40 shrink-0"
-                                    fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                </svg>
+                                <Icon icon="material-symbols:shield-outline" width="16" height="16"
+                                    class="text-base-content/40 shrink-0" />
                                 <input :type="showConfirmPassword ? 'text' : 'password'"
                                     class="grow bg-transparent outline-none text-sm" placeholder="Confirm new password"
                                     v-model="form.password_confirmation" />
                                 <button type="button" @click="showConfirmPassword = !showConfirmPassword"
                                     class="text-base-content/30 hover:text-base-content/60 transition-colors">
-                                    <svg v-if="!showConfirmPassword" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4"
-                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                    </svg>
-                                    <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
-                                    </svg>
+                                    <Icon v-if="!showConfirmPassword" icon="material-symbols:visibility-outline" width="16" height="16" />
+                                    <Icon v-else icon="material-symbols:visibility-off-outline" width="16" height="16" />
                                 </button>
                             </label>
                             <span v-if="form.errors.password_confirmation"
@@ -137,11 +103,7 @@
                             </span>
                             <span v-else class="flex items-center gap-2">
                                 Reset Password
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                                </svg>
+                                <Icon icon="material-symbols:arrow-forward-rounded" width="16" height="16" />
                             </span>
                         </button>
                     </form>
@@ -151,20 +113,12 @@
                     <div class="flex flex-col items-center gap-2 text-sm text-base-content/50">
                         <Link :href="route('password.request')"
                             class="flex items-center gap-1.5 link link-primary font-semibold no-underline hover:underline">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                            </svg>
+                            <Icon icon="material-symbols:lock-open-outline" width="16" height="16" />
                             Request another link
                         </Link>
                         <Link :href="route('login')"
                             class="flex items-center gap-1.5 link link-primary font-semibold no-underline hover:underline">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                            </svg>
+                            <Icon icon="material-symbols:arrow-back-rounded" width="16" height="16" />
                             Back to Login
                         </Link>
                     </div>
@@ -180,6 +134,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useForm, Link } from '@inertiajs/vue3'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps({
     token: String,
