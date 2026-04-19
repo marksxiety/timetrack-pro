a9836136bac784c93033c6fb267704e4398dcad5
Mark Ciril <markciril.jamon@gmail.com>
2026-04-19 19:06:52 +0800

    fix: ensure dark mode configuration is correctly set in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
+    darkMode: ['selector', '[data-theme="dark"]'],
     theme: {
         extend: {},
     },
