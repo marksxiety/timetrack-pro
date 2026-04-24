c6443692ed720a60611d7f904594a6bd236c44ae
Mark Ciril <markciril.jamon@gmail.com>
2026-04-24 18:14:17 +0800

    chore: add jsdoc typing to shift api fetch helper

--- resources/js/Pages/api/shift.js
+++ resources/js/Pages/api/shift.js
@@ -1,3 +1,8 @@
+/**
+ * Fetch the list of shift codes for a given auth role.
+ * @param {'approver'|'employee'} auth
+ * @returns {Promise<Object>}
+ */
 export async function fetchShiftList(auth) {
     const res = await fetch(`/${auth}/shift/list`);
     return res.json();
