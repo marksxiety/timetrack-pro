44415e54f8f702cb962545dade221a55d928c1de
Mark Ciril <markciril.jamon@gmail.com>
2026-04-24 18:15:44 +0800

    chore: add jsdoc typing to overtime stats mapper

--- resources/js/Pages/utils/overtimeMapper.js
+++ resources/js/Pages/utils/overtimeMapper.js
@@ -1,3 +1,22 @@
+/**
+ * @typedef {Object} OvertimeRequest
+ * @property {string} status
+ * @property {number} hours
+ */
+
+/**
+ * @typedef {Object} OvertimeStats
+ * @property {string} totalovertimehours
+ * @property {number} approvedrequests
+ * @property {number} pendingrequests
+ * @property {number} rejectedrequests
+ */
+
+/**
+ * Compute overtime statistics from an array of requests.
+ * @param {OvertimeRequest[]} requests
+ * @returns {OvertimeStats}
+ */
 export function getEmployeeOvertimeStats(requests) {
     let totalovertimehours = 0;
     let pendingrequests = 0;
@@ -13,7 +32,7 @@ export function getEmployeeOvertimeStats(requests) {
             rejectedrequests++;
         }
 
-        if (item.status.toUpperCase() === "APPROVED" || item.status.toUpperCase() === "FILED" ) {
+        if (item.status.toUpperCase() === "APPROVED" || item.status.toUpperCase() === "FILED") {
             approvedrequests++;
             totalovertimehours += item.hours;
         }
