f92116dde5396a5f9e7cc68f42bc4b63cd1c732b
Mark Ciril <markciril.jamon@gmail.com>
2026-04-24 18:14:18 +0800

    chore: add jsdoc typing to heatmap api fetch helper

--- resources/js/Pages/api/heatmap.js
+++ resources/js/Pages/api/heatmap.js
@@ -1,3 +1,11 @@
+/**
+ * Fetch heatmap data for the overtime visualization.
+ * @param {string} startDate - "YYYY-MM-DD"
+ * @param {string} endDate - "YYYY-MM-DD"
+ * @param {string[]} statuses - array of status filters
+ * @param {AbortSignal} [signal] - optional abort controller signal
+ * @returns {Promise<Object>}
+ */
 export async function fetchHeatmapData(startDate, endDate, statuses, signal) {
     const params = new URLSearchParams({
         start_date: startDate,
