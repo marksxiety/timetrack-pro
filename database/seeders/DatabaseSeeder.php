318ef6e602e662881ad27e95e21da48cb8fa83a2
Mark Ciril <markciril.jamon@gmail.com>
2026-04-26 20:36:56 +0800

    ref: remove unused seeders and clean up DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,7 +10,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(OrganizationUnitSeeder::class);
-        $this->call(ShiftCodeSeeder::class);
-        $this->call(OvertimeRequestSeeder::class);
     }
 }
